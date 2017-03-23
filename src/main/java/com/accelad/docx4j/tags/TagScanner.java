84635a096ab45f6ebd653107a566f06f71aeeee7
Romain <romain.malet@accelad.com>
2017-03-23 16:43:39 +0100

    Fix the way to extract tags from Paragraphs
    Use a regular expression to verify if a paragraph is a tag or contains a tag.
    This expression is also used to extract WordItems when a Paragraph contains a tag.
    The content of a Paragraph can be composed with multiple or a single WordItem for a tag:
    - three WordItem: ${ + tag name + }
    - single WordItem: ${tag name}

--- src/main/java/com/accelad/docx4j/tags/TagScanner.java
+++ src/main/java/com/accelad/docx4j/tags/TagScanner.java
@@ -1,5 +1,8 @@
 package com.accelad.docx4j.tags;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.accelad.docx4j.facade.IndexedWordItems;
 import com.accelad.docx4j.facade.Paragraph;
 import com.accelad.docx4j.facade.Paragraphs;
@@ -9,23 +12,26 @@ import com.accelad.docx4j.facade.WordItems;
 
 public class TagScanner {
 
-    private static final String TAG_END = "}";
     private static final String TAG_START = "${";
+    private static final String TAG_END = "}";
+
+    private static final String REGEX = "(\\$\\{[a-zA-Z]*\\})";
+    private static final Pattern PATTERN = Pattern.compile(REGEX);
 
     public Tags scan(Paragraphs paragraphs) {
-
         Tags tags = new Tags();
         for (Paragraph paragraph : paragraphs) {
-            String txt = paragraph.asString();
-
-            if (txt.startsWith(TAG_START)) {
-                TagName tagName = createTagName(txt);
+            String paragraphAsString = paragraph.asString();
+            Matcher match = PATTERN.matcher(paragraphAsString);
+            if (match.matches()) {
+                TagName tagName = createTagName(paragraphAsString);
                 IndexedWordItems content = paragraph.getIndexedContent();
                 Tag tag = new Tag(tagName, paragraph, content);
                 tags.add(tag);
-            } else if (txt.contains(TAG_START)) {
+            } else if (match.find()) {
+                String tagFound = match.group(0);
+                TagName tagName = createTagName(tagFound);
                 IndexedWordItems items = getWordItemForTag(paragraph);
-                TagName tagName = getTagName(paragraph);
                 tags.add(new Tag(tagName, paragraph, items));
             }
         }
@@ -33,53 +39,31 @@ public class TagScanner {
     }
 
     private TagName createTagName(String txt) {
-        txt = txt.substring(2, txt.length() - 1);
-        TagName tagName = new TagName(txt);
-        return tagName;
-    }
-
-    private TagName getTagName(Paragraph paragraph) {
-        StringBuilder stringBuffer = new StringBuilder();
-        boolean started = false;
-
-        for (WordItem item : paragraph.getContent()) {
-            String extractedText = ToStringConverter.toString(item);
-
-            if (extractedText.equals(TAG_START)) {
-                started = true;
-            }
-
-            if (started) {
-                stringBuffer.append(extractedText);
-            }
-
-            if (started && extractedText.equals(TAG_END)) {
-                started = false;
-            }
-        }
-        return createTagName(stringBuffer.toString());
+        String tag = txt.substring(TAG_START.length(), txt.length() - TAG_END.length());
+        return new TagName(tag);
     }
 
     private IndexedWordItems getWordItemForTag(Paragraph paragraph) {
         boolean started = false;
-
         IndexedWordItems items = new IndexedWordItems();
-
-        WordItems content = paragraph.getContent(); // the order matter !
+        WordItems content = paragraph.getContent();
+        // the order of conditions matter !
         for (WordItem item : content) {
             Integer position = paragraph.getPosition(item);
-
             String txt = ToStringConverter.toString(item);
-            if (txt.equals(TAG_START)) {
-                started = true;
-            }
-
-            if (started) {
+            Matcher match = PATTERN.matcher(txt);
+            if (match.matches()) {
                 items.put(item, position);
-            }
-
-            if (started && txt.equals(TAG_END)) {
-                started = false;
+            } else {
+                if (txt.equals(TAG_START)) {
+                    started = true;
+                }
+                if (started) {
+                    items.put(item, position);
+                }
+                if (started && txt.equals(TAG_END)) {
+                    started = false;
+                }
             }
         }
         return items;
---
+++ src/test/java/com/accelad/docx4j/tags/TagScannerTest.java
@@ -0,0 +1,141 @@
+package com.accelad.docx4j.tags;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import com.accelad.docx4j.facade.ListWordItems;
+import com.accelad.docx4j.facade.Paragraph;
+import com.accelad.docx4j.facade.Paragraphs;
+import com.accelad.docx4j.facade.Run;
+import com.accelad.docx4j.facade.Text;
+import com.accelad.docx4j.facade.WordItem;
+import com.accelad.docx4j.facade.WordItems;
+
+public class TagScannerTest {
+
+    private static final String TAG_NAME = "tagName";
+    private static final String A_PARAGRAPH_WITH_TAG_NAME = "A paragraph ${" + TAG_NAME + "} tagged";
+
+    @Mock private Paragraph aParagraph;
+
+    private TagScanner tagScanner;
+
+    @Before
+    public void initialize() {
+        tagScanner = new TagScanner();
+
+        initMocks(this);
+        ListWordItems listWordItems = new ListWordItems();
+        when(aParagraph.getContent()).thenReturn(listWordItems);
+    }
+
+    @Test
+    public void should_create_tag_with_correct_tag_name_when_a_paragraph_contains_a_tag_name() {
+        Paragraphs paragraphs = new Paragraphs();
+        when(aParagraph.asString()).thenReturn(A_PARAGRAPH_WITH_TAG_NAME);
+        paragraphs.add(aParagraph);
+
+        Tags tags = tagScanner.scan(paragraphs);
+
+        Tag tag = tags.iterator().next();
+        assertThat(tag.getTagName().asString(), is(TAG_NAME));
+    }
+
+    @Test
+    public void should_create_tag_with_correct_tag_name_when_a_paragraph_contains_only_a_tag_name() {
+        Paragraphs paragraphs = new Paragraphs();
+        when(aParagraph.asString()).thenReturn("${" + TAG_NAME + "}");
+        paragraphs.add(aParagraph);
+
+        Tags tags = tagScanner.scan(paragraphs);
+
+        Tag tag = tags.iterator().next();
+        assertThat(tag.getTagName().asString(), is(TAG_NAME));
+    }
+
+    @Test
+    public void should_return_empty_tags_when_no_paragraph_contains_tag_identifier() {
+        Paragraphs paragraphs = new Paragraphs();
+        when(aParagraph.asString()).thenReturn("No tag in paragraph");
+        paragraphs.add(aParagraph);
+
+        Tags tags = tagScanner.scan(paragraphs);
+
+        assertFalse(tags.iterator().hasNext());
+    }
+
+    @Test
+    public void should_create_tag_with_one_word_item_when_tag_identifier_is_a_word_item() {
+        Paragraphs paragraphs = new Paragraphs();
+        when(aParagraph.asString()).thenReturn(A_PARAGRAPH_WITH_TAG_NAME);
+        paragraphs.add(aParagraph);
+        ListWordItems wordItems = new ListWordItems();
+        Run wordItem1 = new Run();
+        wordItem1.addText(new Text("A paragraph "));
+        Run expectedItem = new Run();
+        expectedItem.addText(new Text("${" + TAG_NAME + "}"));
+        Run wordItem3 = new Run();
+        wordItem3.addText(new Text(" tagged"));
+        wordItems.add(wordItem1);
+        wordItems.add(expectedItem);
+        wordItems.add(wordItem3);
+        when(aParagraph.getContent()).thenReturn(wordItems);
+
+        Tags tags = tagScanner.scan(paragraphs);
+
+        Tag tag = tags.iterator().next();
+        WordItems items = tag.getItems();
+        Iterator<WordItem> iterator = items.iterator();
+        assertThat(iterator.next(), is(expectedItem));
+        assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void should_create_tag_with_word_items_when_tag_identifier_is_composed_by_multiple_word_item() {
+        Paragraphs paragraphs = new Paragraphs();
+        when(aParagraph.asString()).thenReturn(A_PARAGRAPH_WITH_TAG_NAME);
+        paragraphs.add(aParagraph);
+        ListWordItems wordItems = new ListWordItems();
+        Run wordItem1 = new Run();
+        wordItem1.addText(new Text("A paragraph "));
+        Run wordItem2 = new Run();
+        wordItem2.addText(new Text("${"));
+        Run wordItem3 = new Run();
+        wordItem3.addText(new Text(TAG_NAME));
+        Run wordItem4 = new Run();
+        wordItem4.addText(new Text("}"));
+        Run wordItem5 = new Run();
+        wordItem5.addText(new Text(" tagged"));
+        wordItems.add(wordItem1);
+        wordItems.add(wordItem2);
+        wordItems.add(wordItem3);
+        wordItems.add(wordItem4);
+        wordItems.add(wordItem5);
+        when(aParagraph.getContent()).thenReturn(wordItems);
+
+        Tags tags = tagScanner.scan(paragraphs);
+
+        Tag tag = tags.iterator().next();
+        List<WordItem> wordItemsActual = getWordItems(tag.getItems());
+        assertThat(wordItemsActual, hasSize(3));
+        assertThat(wordItemsActual, hasItems(wordItem2, wordItem3, wordItem4));
+    }
+
+    private List<WordItem> getWordItems(WordItems items) {
+        return StreamSupport.stream(items.spliterator(), false).collect(Collectors.toList());
+    }
+}
